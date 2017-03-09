1debc263ce5c3edcfd1c8cc1b512e92889f9720b
bas2 <me@bashir.biz>
2017-03-09 18:40:29 +0000

    Added script.js ref.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
   {!! Html::script('js/jquery/jquery-3.1.1.min.js') !!}
   {!! Html::script('js/imghover.js') !!}
   {!! Html::script('js/slimbox/slimbox2.js') !!}
+  {!! Html::script('js/script.js') !!}
 </head>
 <body>
   @yield('content')
