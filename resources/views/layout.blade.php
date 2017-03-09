f567bdc7d1de2461ccf278e7f110f603ca25600f
bas2 <me@bashir.biz>
2017-03-09 18:32:53 +0000

    New commit.

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>{{ $pagetitle }}</title>
+  {!! Html::style('css/styles.css') !!}
+  {!! Html::style('js/slimbox/slimbox2.css') !!}
+
+  {!! Html::script('js/jquery/jquery-3.1.1.min.js') !!}
+  {!! Html::script('js/imghover.js') !!}
+  {!! Html::script('js/slimbox/slimbox2.js') !!}
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
