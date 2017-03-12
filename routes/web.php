4d9271d4a04477cf2726106da80f8356a1002712
bas2 <me@bashir.biz>
2017-03-12 19:12:01 +0000

    Modified text in Skills section.

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/home', function () {
   $skills_list=[
   'PHP'               =>'I have been using PHP since the year 2000. I am therefore knowledgable with this scripting language. I use OOP extensively and have recently started using the <a href="https://laravel.com/">Laravel</a> PHP framework.',
   'MySQL'             =>'Like PHP, I have been using this database system since 2000.',
-  'HTML'              =>'I have using HTML since 1999.',
+  'HTML'              =>'I have been using HTML since 1999.',
   'CSS'               =>'I quickly picked up on the benefits of CSS quite soon after learning to code HTML.',
   'Jquery'            =>'I recently started using this in favour of plain Javascript in the last few years.',
   'Linux server admin'=>'Since electing to use Ubuntu instead of Windows as my chosen desktop OS, I have developed a liking for the Linux operating system. My domain bashir.biz was hosted on a CentOS server on which I had configured web, mail and database servers.',
