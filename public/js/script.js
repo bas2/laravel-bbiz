0cc3ccf6509221088575bed623e1c025e6cee29d
bas2 <me@bashir.biz>
2017-03-12 19:09:42 +0000

    Added jquery

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  
+  if ($('p.error').length==0&&$('p.success').length==0) { 
+    $('<p><a href="#">Contact me</a></p>').insertBefore('.intro div').click(function(){
+      $('.intro div').show();
+      $(this).hide();
+      $('input[name=message]').focus();
+    });
+    $('.intro div').hide();
+  } else {
+    if ($('p.success').length>0) {
+      $('.intro div form').hide();
+      $('.intro div').delay(3000).fadeOut();
+    }
+  }
 });
