69866c3f6f722acfb422fd92b7ca7d5f3cf3caff
bas2 <me@bashir.biz>
2017-03-14 16:41:53 +0000

    Changed table name to plural.

--- database/migrations/2017_03_14_153310_create_content_table.php
+++ database/migrations/2017_03_14_153310_create_content_table.php
@@ -13,7 +13,7 @@ class CreateContentTable extends Migration
      */
     public function up()
     {
-        Schema::create('content', function (Blueprint $table) {
+        Schema::create('contents', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->default('');
             $table->text('content');
