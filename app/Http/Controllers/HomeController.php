cded09ec810610f90a5197c01ea450072e142f12
bas2 <me@bashir.biz>
2017-03-14 16:59:35 +0000

    created getsection method to get content from database. Changes owing to moving views to pages subfolder. Created update method. Created updatecontent. Created updatesection as a helper method.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,6 @@ use App\Mail\ContactMail;
 class HomeController extends Controller
 {
   public function index() {
-    $aboutme='<p>I am a Web developer based in London, UK. I am passionate about developing for the Web. I have spent a significant period of time learning the tools of the web to a high standard.</p>';
     $skills_list=[
     'PHP'               =>'I have been using PHP since the year 2000. I am therefore knowledgable with this scripting language. I use OOP extensively and have recently started using the <a href="https://laravel.com/">Laravel</a> PHP framework.',
     'MySQL'             =>'Like PHP, I have been using this database system since 2000.',
@@ -18,11 +17,11 @@ class HomeController extends Controller
     'Jquery'            =>'I recently started using this in favour of plain Javascript in the last few years.',
     'Linux server admin'=>'Since electing to use Ubuntu instead of Windows as my chosen desktop OS, I have developed a liking for the Linux operating system. My domain bashir.biz was hosted on a CentOS server on which I had configured web, mail and database servers.',
     ];
-    $recent='I have just completed work on the website and web applications at <a href="http://www.hcdchauffeurdrive.com/">HCD Chauffeur drive</a>. They are a chauffeur company based in London. I was asked to redevelop their web site and to build their online booking system and controller/driver systems.';
     $images=['hcdsite','hcdbooking2','hcdbooking','hcdcapp','hcdapp2'];
-    return view('welcome')
+    return view('pages.welcome')
     ->with('page',['home','Bashir Patel (Web developer/programmer) London-based'])
-    ->with('pagecontent',['aboutme'=>$aboutme,'skill'=>$skills_list,'recent'=>$recent])
+    ->with('pagecontent',
+      ['aboutme'=>$this->getsection('about'),'skill'=>$skills_list,'recent'=>$this->getsection('recent')])
     ->with('images',$images);
   }
 
@@ -37,10 +36,42 @@ class HomeController extends Controller
   }
 
   public function projects() {
-    return view('projects')->with('page',['projects','Projects'])
+    return view('pages.projects')->with('page',['projects','Projects']);
+  }
+
+  public function update() {
+    $about=\App\Content::where('name','about')->get();
+    $recent=\App\Content::where('name','recent')->get();
+    $recent=($recent->count()==1) ? $recent[0]->content : '' ;
+    return view('pages.update')->with('page',['update','Update'])
+    ->with('content',['about'=>$about[0]->content,'recent'=>$recent])
     ;
   }
 
+  private function getsection($section) {
+    $about=\App\Content::where('name',$section)->get();
+    return $about[0]->content;
+  }
 
+  private function updatesection($section, Request $request) {
+    $about=\App\Content::where('name',$section)->get();
+    if($about->count()==0) {
+      $create= new \App\Content;
+      $create->name = $section;
+      $create->content = '';
+      $create->save();
+    }
+    $input=$request->all();
+    $update=\App\Content::where('name',$section)->update(['content'=>$input[$section]]);
+  }
+
+  public function updatecontent(Request $request) {
+    $this->updatesection('about', $request);
+    $this->updatesection('recent', $request);
+
+
+
+    return redirect('home');
+  }
 
 }
