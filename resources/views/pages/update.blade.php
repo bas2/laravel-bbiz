0d598982fe82a83fb27e3aad7cc3c04a88026a26
bas2 <me@bashir.biz>
2017-03-14 21:11:09 +0000

    Modified form for file uploads. Added options for updating skills section.

--- resources/views/pages/update.blade.php
+++ resources/views/pages/update.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('content')
-{!! Form::open(['route'=>'updatecontent']) !!}
+{!! Form::open(['route'=>'updatecontent','method'=>'POST','files'=>'true']) !!}
 <h1>About me</h1>
 <div>
 {!! Form::textarea('about',$content['about'],['required']) !!}
@@ -11,7 +11,18 @@
 {!! Form::textarea('recent',$content['recent'],['required']) !!}
 </div>
 
+{!! Form::file('image') !!}
 
+<h1>Skills</h1>
+@foreach ($content['skills'] as $skill)
+<div>
+{!! Form::text("skillname_".$skill->id,$skill->skill) !!}
+{!! Form::text("skillcontent_".$skill->id,$skill->content) !!}
+</div>
+@endforeach
+<div>
+{!! Form::text("skillname",null,['placeholder'=>'Enter new skill name']) !!}
+</div>
 
 <div>
 {!! Form::submit('Update >') !!}
