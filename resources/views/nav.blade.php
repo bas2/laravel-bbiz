1d21e33336b7c1ed32ba272b370c236679a2e98f
bas2 <me@bashir.biz>
2017-03-14 15:42:16 +0000

    Added Update link to update page content.

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,4 +1,5 @@
 <ul class="nav">
   <li>{{ link_to('home', 'Home',  ['class'=>'homelink']) }}
   <li>{{ link_to('projects','Projects',['class'=>'projectslink']) }}
+  <li>{{ link_to('update','Update',['class'=>'updatelink']) }}
 </ul>
