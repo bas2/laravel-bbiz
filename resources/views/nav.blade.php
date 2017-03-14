ba93113e2532336a1891d8983c2ec1f661abb993
bas2 <me@bashir.biz>
2017-03-14 16:41:08 +0000

    Changed link to content/update.

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,5 @@
 <ul class="nav">
   <li>{{ link_to('home', 'Home',  ['class'=>'homelink']) }}
   <li>{{ link_to('projects','Projects',['class'=>'projectslink']) }}
-  <li>{{ link_to('update','Update',['class'=>'updatelink']) }}
+  <li>{{ link_to('content/update','Update',['class'=>'updatelink']) }}
 </ul>
