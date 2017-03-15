b04bf1684f8eb131aa4fb8e2e81124f0989666f5
bas2 <me@bashir.biz>
2017-03-15 16:11:42 +0000

    Added routes for login/logout functionality.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ Route::get('projects',        'HomeController@projects');
 Route::get('content/update',  'HomeController@update')->name('updatecontent');
 Route::post('content/update', 'HomeController@updatecontent')->name('updatecontent');
 
+Route::get('login', 'HomeController@getlogin')->name('getlogin');
+Route::post('login', 'HomeController@postlogin')->name('postlogin');
+Route::get('logout', 'HomeController@getlogout')->name('getlogout');
+
 Route::get( '{slug?}',    'HomeController@index')->where('slug','(home)') # Matches / or /home.
 ->name('home');
 Route::post('email/send', 'HomeController@sendEmail')->name('sendcontacemeil'); # Send email.
