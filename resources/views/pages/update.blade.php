bb72cf2c922bb1372aa729aa2c1daad3f239d49a
bas2 <me@bashir.biz>
2017-03-15 00:09:33 +0000

    Added fieldsets. Changed skills content to textarea. Moved recent work to bottom.

--- resources/views/pages/update.blade.php
+++ resources/views/pages/update.blade.php
@@ -1,31 +1,52 @@
 @extends('layout')
 @section('content')
 {!! Form::open(['route'=>'updatecontent','method'=>'POST','files'=>'true']) !!}
-<h1>About me</h1>
+
+<fieldset class="intro">
+<legend>About me</legend>
 <div>
 {!! Form::textarea('about',$content['about'],['required']) !!}
 </div>
+</fieldset>
 
-<h1>Recent work</h1>
-<div>
-{!! Form::textarea('recent',$content['recent'],['required']) !!}
-</div>
 
-{!! Form::file('image') !!}
-
-<h1>Skills</h1>
+<fieldset class="skills">
+<legend>Skills</legend>
 @foreach ($content['skills'] as $skill)
 <div>
 {!! Form::text("skillname_".$skill->id,$skill->skill) !!}
-{!! Form::text("skillcontent_".$skill->id,$skill->content) !!}
+{!! Form::textarea("skillcontent_".$skill->id,$skill->content) !!}
 </div>
 @endforeach
 <div>
 {!! Form::text("skillname",null,['placeholder'=>'Enter new skill name']) !!}
 </div>
 
+
+</fieldset>
+
+<fieldset class="recent">
+<legend>Recent work</legend>
+<div>
+{!! Form::textarea('recent',$content['recent'],['required']) !!}
+</div>
+
+{!! Form::file('image') !!}
+
+<fieldset>
+<legend>Current images</legend>
+<ul>
+@foreach ($images as $image)
+<li>{{ Html::image("img/$image->filename",'',['width'=>200]) }}
+@endforeach
+</ul>
+</fieldset>
+
+</fieldset>
+
 <div>
 {!! Form::submit('Update >') !!}
 </div>
+
 {!! Form::close() !!}
 @endsection
