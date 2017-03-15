9c38b6a0513d3fe5ec74edc305bed7be18bcbe12
bas2 <me@bashir.biz>
2017-03-15 16:51:38 +0000

    Put nav links inside Auth::check() condition. Included a Logout option. Finally commented out Login option as that is not needed on production site.

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,10 @@
 <ul class="nav">
+  @if(\Auth::check())
   <li>{{ link_to('home', 'Home',  ['class'=>'homelink']) }}
   <li>{{ link_to('projects','Projects',['class'=>'projectslink']) }}
   <li>{{ link_to('content/update','Update',['class'=>'updatelink']) }}
+  <li>{{ link_to('logout','Logout',['class'=>'logoutlink']) }}
+  @else
+  {{-- <li>{{ link_to('login','Login',['class'=>'loginlink']) }} --}}
+  @endif
 </ul>
