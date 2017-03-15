62f7ffec7b4f4f63b3148f23f0b9d41ca0f78c7e
bas2 <me@bashir.biz>
2017-03-15 00:04:08 +0000

    Changes owing to adding contactform class attribute to div surrounding contactform. Focus name field in form instead of message.

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
   if ($('p.error').length==0&&$('p.success').length==0) { 
-    $('<p><a href="#">Contact me</a></p>').insertBefore('.intro div').click(function(e){
-      $('.intro div').show();
+    $('<p><a href="#">Contact me</a></p>').insertBefore('#home .intro div.contactform').click(function(e){
+      $('#home .intro div.contactform').show();
       $(this).hide();
-      $('input[name=message]').focus();
+      $('input[name=name]').focus();
       e.preventDefault();
     });
-    $('.intro div').hide();
+    $('#home .intro div.contactform').hide();
   } else {
     if ($('p.success').length>0) {
-      $('.intro div form').hide();
-      $('.intro div').delay(3000).fadeOut();
+      $('#home .intro div.contactform form').hide();
+      $('#home .intro div.contactform').delay(3000).fadeOut();
     }
   }
 });
