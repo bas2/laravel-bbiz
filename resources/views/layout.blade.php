5f4f7b0273b6498ef2184cecb363655f33bd6f1b
bas2 <me@bashir.biz>
2017-03-21 01:27:14 +0000

    Removed imghover.js reference.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,6 @@
   {!! Html::style('css/styles.css') !!}
 
   {!! Html::script('js/jquery/jquery-3.1.1.min.js') !!}
-  {!! Html::script('js/imghover.js') !!}
   {!! Html::script('js/script.js') !!}
 </head>
 <body id="{{ $page[0] }}">
