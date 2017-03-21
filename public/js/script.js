790fb80bf01f03cba2ee0ec8236fc7420e39a9a0
bas2 <me@bashir.biz>
2017-03-21 01:13:27 +0000

    Added simple overlay script.

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   if ($('p.error').length==0&&$('p.success').length==0) { 
     $('<p><a href="#">Contact me</a></p>').insertBefore('#home .intro div.contactform').click(function(e){
       $('#home .intro div.contactform').show();
@@ -13,4 +14,18 @@ $(document).ready(function(){
       $('#home .intro div.contactform').delay(3000).fadeOut();
     }
   }
+
+
+  $('.overlay').click(function(e){
+    $('<div class="overlay_container"><div>Close</div><img src="'+$(this).find('img').attr('src')+'"></div>')
+    .appendTo('body');
+    $("body").css("overflow","hidden");
+    e.preventDefault();
+  });
+
+  $(document).on('click','.overlay_container div',function(){
+    $(this).parent().remove();
+    $("body").css("overflow","auto");
+  });
+
 });
