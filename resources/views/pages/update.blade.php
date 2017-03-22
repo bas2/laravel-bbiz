2ea4ff6b0fcf934c806b546c3420ed183b32351d
bas2 <me@bashir.biz>
2017-03-22 23:46:07 +0000

    Changed fieldsets into divs. Added page menu to anchor each section. Added content updated status message. Added container and row for each section. Added bootstrap classes to form elements. Added panels for each section.

--- resources/views/pages/update.blade.php
+++ resources/views/pages/update.blade.php
@@ -2,53 +2,100 @@
 @section('content')
 {!! Form::open(['route'=>'updatecontent','method'=>'POST','files'=>'true']) !!}
 
-<fieldset class="intro">
-<legend>About me</legend>
 <div>
-{!! Form::textarea('about',$content['about'],['required']) !!}
-</div>
-<div>
-{!! Form::email('email',$content['email']) !!}
-</div>
-</fieldset>
 
-
-<fieldset class="skills">
-<legend>Skills</legend>
-@foreach ($content['skills'] as $skill)
-<div>
-{!! Form::text("skillname_".$skill->id,$skill->skill) !!}
-{!! Form::textarea("skillcontent_".$skill->id,$skill->content) !!}
-</div>
-@endforeach
-<div>
-{!! Form::text("skillname",null,['placeholder'=>'Enter new skill name']) !!}
-</div>
-
-
-</fieldset>
-
-<fieldset class="recent">
-<legend>Recent work</legend>
-<div>
-{!! Form::textarea('recent',$content['recent'],['required']) !!}
-</div>
-
-{!! Form::file('image') !!}
-
-<fieldset>
-<legend>Current images</legend>
-<ul>
-@foreach ($images as $image)
-<li>{{ Html::image("img/$image->filename",'',['width'=>200]) }}
-@endforeach
+<ul class="list-inline">
+<li><a href="#about">About me</a>
+<li><a href="#skills">Skills history</a>
+<li><a href="#recent">Recent work</a>
 </ul>
-</fieldset>
 
-</fieldset>
-
+@if($flash=session('message'))
+<div class="alert alert-success alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session('message') }}</div>
+@endif
+<div class="container-fluid">
+<div class="row">
 <div>
-{!! Form::submit('Update >') !!}
+{!! Form::submit('Update >',['class'=>'btn btn-lg btn-primary btn-block']) !!}
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+
+  <div id="intro" class="col-md-12 intro">
+    <div class="panel hunp">
+      <h2 class="panel-heading">About me</h2>
+      <div class="container">
+      <div>
+      {!! Form::textarea('about',$content['about'],['class'=>'form-control','required']) !!}
+      </div>
+      <div>
+      {!! Form::email('email',$content['email'],['class'=>'form-control','placeholder'=>'Email for contact form']) !!}
+      </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div id="skills" class="col-md-12 skills">
+    <div class="panel hunp">
+      <h2 class="panel-heading">Skills</h2>
+      <div class="container">
+      @foreach ($content['skills'] as $skill)
+      <div>
+      {!! Form::text("skillname_".$skill->id,$skill->skill,['class'=>'form-control']) !!}
+      {!! Form::textarea("skillcontent_".$skill->id,$skill->content,['class'=>'form-control']) !!}
+      </div>
+      @endforeach
+      <div>
+      {!! Form::text("skillname",null,['class'=>'form-control','placeholder'=>'Enter new skill name']) !!}
+      </div>
+      </div>
+    </div>
+  </div>
+
+
+</div>
+</div>
+
+
+<div class="container-fluid">
+<div class="row">
+
+  <div id="recent" class="col-md-12 recent">
+    <div class="panel hunp">
+      <h2 class="panel-heading">Recent work</h2>
+      <div class="container">
+      <div>
+      {!! Form::textarea('recent',$content['recent'],['class'=>'form-control','required']) !!}
+      </div>
+
+      {!! Form::file('image') !!}
+
+      <h3>Current images</h3>
+      <ul class="list-inline">
+      @foreach ($images as $image)
+      <li>{{ Html::image("img/$image->filename",'',['width'=>200]) }}
+      @endforeach
+      </ul>
+      </div>
+    </div>
+  </div>
+
+</div>
+</div>
+
+
+<div class="container-fluid">
+<div class="row">
+<div>
+{!! Form::submit('Update >',['class'=>'btn btn-lg btn-primary btn-block']) !!}
+</div>
+</div>
+</div>
+
 </div>
 
 {!! Form::close() !!}
