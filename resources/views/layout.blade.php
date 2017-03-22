53a4e1c5d091ab8420789318e2d76ac407d50e58
bas2 <me@bashir.biz>
2017-03-22 16:50:49 +0000

    Added meta viewport. Added boostrap css and js. Added a wrapper after the nav layout. Added footer.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,10 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ $page[1] }}</title>
+  <!-- Bootstrap -->
+  {!! Html::style('css/bootstrap.min.css') !!}
   {!! Html::style('css/styles.css') !!}
 
   {!! Html::script('js/jquery/jquery-3.1.1.min.js') !!}
@@ -10,6 +13,15 @@
 </head>
 <body id="{{ $page[0] }}">
   @include('nav')
+  <div class="wrapper">
   @yield('content')
+  </div>
+    <footer class="footer">
+      <div class="container-fluid">
+        <p class="text-muted text-center">Glyphicons provided by {{ link_to('http://glyphicons.com','<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span> Glyphicons',[],null,false) }}</p>
+
+      </div>
+    </footer>
+  {!! Html::script('js/bootstrap.min.js') !!}
 </body>
 </html>
