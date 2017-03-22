2a150261d6131dfcbaec6145c3e871ea69f6e9f5
bas2 <me@bashir.biz>
2017-03-22 16:52:04 +0000

    Added glyphicons to nav bar.

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,10 +1,10 @@
-<ul class="nav">
+<ul class="nav navbar-inverse nav-justified">
   @if(\Auth::check())
-  <li>{{ link_to('home', 'Home',  ['class'=>'homelink']) }}
+  <li>{{ link_to('home', '<span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home',  ['class'=>'homelink'],null,false) }}
   <li>{{ link_to('projects','Projects',['class'=>'projectslink']) }}
-  <li>{{ link_to('content/update','Update',['class'=>'updatelink']) }}
-  <li>{{ link_to('logout','Logout',['class'=>'logoutlink']) }}
+  <li>{{ link_to('content/update','<span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Update',['class'=>'updatelink'],null,false) }}
+  <li>{{ link_to('logout','<span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Logout',['class'=>'logoutlink'],null,false) }}
   @else
-  <li>{{ link_to('login','Login',['class'=>'loginlink']) }}
+  <li>{{ link_to('login','<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span> Login',['class'=>'loginlink'],null,false) }}
   @endif
 </ul>
