21e3824fba870fe293482a853b1c322176d22765
bas2 <me@bashir.biz>
2017-03-22 16:54:05 +0000

    Added div inside form so I could contrain the width of form elements. Added boostrap classes.

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,18 +1,20 @@
 @extends('layout')
 @section('content')
+
 {!! Form::open() !!}
-
 <div>
-{!! Form::text('username',null,['placeholder'=>'Your username...']) !!}
+<div>
+{!! Form::text('username',null,['class'=>'form-control','placeholder'=>'Your username...']) !!}
 </div>
 
 <div>
-{!! Form::password('password',null,['placeholder'=>'Your password...']) !!}
+{!! Form::password('password',['class'=>'form-control','placeholder'=>'Your password...']) !!}
 </div>
 
 <div>
-{!! Form::submit('Login >') !!}
+{!! Form::submit('Login >',['class'=>'btn btn-lg btn-primary btn-block']) !!}
 </div>
-
+</div>
 {!! Form::close() !!}
+
 @endsection
