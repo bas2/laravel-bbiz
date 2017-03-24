b9719e82ee8233b631a9118d819810e5b27f7f7e
bas2 <me@bashir.biz>
2017-03-24 19:10:28 +0000

    Added form-group classes to divs containing form controls to make the form look better.

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -3,15 +3,15 @@
 
 {!! Form::open() !!}
 <div>
-<div>
+<div class="form-group">
 {!! Form::text('username',null,['class'=>'form-control','placeholder'=>'Your username...']) !!}
 </div>
 
-<div>
+<div class="form-group">
 {!! Form::password('password',['class'=>'form-control','placeholder'=>'Your password...']) !!}
 </div>
 
-<div>
+<div class="form-group">
 {!! Form::submit('Login >',['class'=>'btn btn-lg btn-primary btn-block']) !!}
 </div>
 </div>
