c3b536b3319e8748320db1a4cc78415b32dc6e39
bas2 <me@bashir.biz>
2017-03-24 19:05:48 +0000

    Added danger class to if condition since we now show a message when an email could not be sent due to empty email address.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  if ($('p.alert-warning').length==0&&$('p.alert-success').length==0) {
+  if ($('p.alert-warning').length==0&&$('p.alert-success').length==0&&$('p.alert-danger').length==0) {
     // Hide form and replace with Contact me link, which when clicked, show the form, hide the 
     // link and focus the name field.
     $('<p><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> <a href="#">Contact me</a></p>').insertBefore('#home .intro div.contactform').click(function(e){
