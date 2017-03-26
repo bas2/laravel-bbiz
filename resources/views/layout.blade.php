9a655c88b9cfcf2d42c35c71f3e9d9b9e542374a
117229 <u88610319@cbi-bs1.server.lan>
2017-03-26 23:32:30 +0200

    Uploaded via Sublime Text.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,16 +2,26 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>{{ $pagetitle }}</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ $page[1] }}</title>
+  <!-- Bootstrap -->
+  {!! Html::style('css/bootstrap.min.css') !!}
   {!! Html::style('css/styles.css') !!}
-  {!! Html::style('js/slimbox/slimbox2.css') !!}
 
   {!! Html::script('js/jquery/jquery-3.1.1.min.js') !!}
-  {!! Html::script('js/imghover.js') !!}
-  {!! Html::script('js/slimbox/slimbox2.js') !!}
   {!! Html::script('js/script.js') !!}
 </head>
-<body>
+<body id="{{ $page[0] }}">
+  @include('nav')
+  <div class="wrapper">
   @yield('content')
+  </div>
+    <footer class="footer">
+      <div class="container-fluid">
+        <p class="text-muted text-center">Glyphicons provided by {{ link_to('http://glyphicons.com','<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span> Glyphicons',[],null,false) }}</p>
+
+      </div>
+    </footer>
+  {!! Html::script('js/bootstrap.min.js') !!}
 </body>
 </html>
