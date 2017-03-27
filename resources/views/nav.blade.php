bcb80d18700c23e20f9896eb8b24ff36b75eb386
bas2 <me@bashir.biz>
2017-03-27 20:35:57 +0100

    Update for local version.

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -16,8 +16,8 @@
       <li>{{ link_to('content/update','<span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Update',['class'=>'updatelink'],null,false) }}
       <li>{{ link_to('logout','<span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Logout',['class'=>'logoutlink'],null,false) }}
       @else
-      <li>{{ link_to('/', '<span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home',  ['class'=>'homelink'],null,false) }}
-      {{-- <li>{{ link_to('login','<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span> Login',['class'=>'loginlink'],null,false) }} --}}
+      <li>{{ link_to('home', '<span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home',  ['class'=>'homelink'],null,false) }}
+      <li>{{ link_to('login','<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span> Login',['class'=>'loginlink'],null,false) }}
       @endif
       </ul>
     </div>
