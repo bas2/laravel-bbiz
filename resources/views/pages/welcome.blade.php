28158e03a6cc85fd621ff453f494e94a11107e68
bas2 <me@bashir.biz>
2017-03-29 17:31:33 +0100

    Travelodge prices.

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -5,31 +5,156 @@
 
 <div class="col-md-12 travelodge-affiliate">
   <div class="panel">
-    <h3 class="panel-heading text-center">Book a room with travelodge</h2>
+    <h3 class="panel-heading text-center">Book a room for one night with Travelodge</h3>
     <div class="panel-body">
-{!! $pagecontent['travelodge'] !!}
+    {!! $travelodge['lead'] !!}
 
-    <h4>Today's lowest prices - Monday, 27 March 2017</h4>
-    <p><small>Online booking available until midnight and check-in is after 3pm on Tuesday.</small></p>
+    <div class="col-md-6">
+    <h4>Today's lowest prices - {{ date('l, j F') }} check-in</h4>
+    <p>Online booking available until <strong>midnight</strong>, <strong>check-in is after 3pm</strong> and <strong>check-out is at midday on {{ \Carbon\Carbon::parse(\Carbon\Carbon::now()->addDay())->format('l') }}</strong>.</p>
     <ul>
-    <li>Both Kingston hotels, Surrey £49</li>
-    <li>Teddington, Surrey £49</li>    
-    <li>Twickenham, Surrey £49 (located next to Twickenham station)</li>
-    <li>London Hounslow, £49</li>
-    <li>London Crystal Palace, £49</li>
-    <li>Whetstone, £52</li>
-    <li>Cheshunt, £60</li>
+      @foreach($travelodge['today'] as $hoteldate)
+      <li>{{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+      @endforeach
     </ul>
-    <p></p>
+    </div>
 
-    <h4>Tomorrow's lowest prices - Tuesday, 27 March 2017</h4>
-    <p><small>Online booking available until midnight on the day and check-in is after 3pm on the day.</small></p>
-    <p></p>
+    <div class="col-md-6">
+    <h4>Tomorrow's lowest prices - {{ \Carbon\Carbon::parse(\Carbon\Carbon::now()->addDay())->format('l, j F') }} check-in</h4>
+    <p>Online booking available until <strong>midnight on {{ \Carbon\Carbon::parse(\Carbon\Carbon::now()->addDay(2))->format('l') }}</strong>, <strong>check-in is after 3pm</strong> and <strong>check-out is at midday on {{ \Carbon\Carbon::parse(\Carbon\Carbon::now()->addDay(2))->format('l') }}</strong>.</p>
+    <ul>
+      @foreach($travelodge['tomorrow'] as $hoteldate)
+      <li>{{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+      @endforeach
+    </ul>
+    </div>
 
-    <h4>Lowest prices at the weekend - Saturday, 1 April 2017 and Sunday 2 April 2017</h4>
-    <p><small>Online booking available until midnight on the day and check-in is after 3pm on the day.</small></p>
-    <p></p>
-    
+    <div class="col-md-12">
+
+      <h4 class="text-center">Lowest prices at the weekend - {{ \Carbon\Carbon::parse('next saturday')->format('l j F') }} and {{ \Carbon\Carbon::parse('next sunday')->format('l j F') }}</h4>
+      <p class="text-center">Online booking available until <strong>midnight of the following day</strong> and <strong>check-in is after 3pm on the day</strong>.</p>
+      
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sat'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sun'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+    </div>
+
+    <div class="col-md-12">
+
+      <h4 class="text-center">Lowest prices at the weekend - {{ \Carbon\Carbon::parse('next saturday')->addWeek()->format('l j F') }} and {{ \Carbon\Carbon::parse('next sunday')->addWeek()->format('l j F') }}</h4>
+      <p class="text-center">Online booking available until <strong>midnight of the following day</strong> and <strong>check-in is after 3pm on the day</strong>.</p>
+      
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sat1'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sun1'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+    </div>
+
+
+
+    <div class="col-md-12">
+
+      <h4 class="text-center">Lowest prices at the weekend - {{ \Carbon\Carbon::parse('next saturday')->addWeek(2)->format('l j F') }} and {{ \Carbon\Carbon::parse('next sunday')->addWeek(2)->format('l j F') }}</h4>
+      <p class="text-center">Online booking available until <strong>midnight of the following day</strong> and <strong>check-in is after 3pm on the day</strong>.</p>
+      
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sat2'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sun2'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+    </div>
+
+
+    <div class="col-md-12">
+
+      <h4 class="text-center">Lowest prices at the weekend - {{ \Carbon\Carbon::parse('next saturday')->addWeek(3)->format('l j F') }} and {{ \Carbon\Carbon::parse('next sunday')->addWeek(3)->format('l j F') }}</h4>
+      <p class="text-center">Online booking available until <strong>midnight of the following day</strong> and <strong>check-in is after 3pm on the day</strong>.</p>
+      
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sat3'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sun3'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+    </div>
+
+
+    <div class="col-md-12">
+
+      <h4 class="text-center">Lowest prices at the weekend - {{ \Carbon\Carbon::parse('next saturday')->addWeek(4)->format('l j F') }} and {{ \Carbon\Carbon::parse('next sunday')->addWeek(4)->format('l j F') }}</h4>
+      <p class="text-center">Online booking available until <strong>midnight of the following day</strong> and <strong>check-in is after 3pm on the day</strong>.</p>
+      
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sat4'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+      <div class="col-md-6">
+      <ul>
+        @foreach($travelodge['sun4'] as $hoteldate)
+        <li>{{ \Carbon\Carbon::parse($hoteldate->date)->format('l') }} {{ $hoteldate->hotels[0]->name }}, <strong>{{ $hoteldate->price }}</strong></li>
+        @endforeach
+      </ul>
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+
 
     </div>
   </div>
