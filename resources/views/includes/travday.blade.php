b9555719c4b6d21f566cc7cc3ec19eb63d475ccf
117229 <u88610319@cbi-bs1.server.lan>
2017-04-03 22:42:57 +0200

    Commit.

--- resources/views/includes/travday.blade.php
+++ resources/views/includes/travday.blade.php
@@ -2,7 +2,11 @@
   <legend>{{ $text }} {{ \Carbon\Carbon::parse($date2)->format('l, j F') }}</legend>
   
   @foreach($travelodge[strtolower(str_replace(' ','',$text))] as $hoteldate)
-  @if( !isset($order) || ( in_array($order, [strtoupper(substr($hoteldate->descr, 0,3)),'TW']) ) )
+  @if( !isset($order) # Initial.
+  || ( $order == 'WALTOL' ) # All.
+  || ( in_array($order, [strtoupper(substr($hoteldate->descr, 0,3))]) ) # 
+  || ( 'WALTOL' == strtoupper($hoteldate->descr) ) # 
+  )
   <div class="form-group">
   {!! Form::select("price_{$hoteldate->date_id}",range(0,70),$hoteldate->price) !!}
   {{ Form::label($hoteldate->hotels[0]->name) }} <span class="zero">x</span> <span class="hoteldescr">{{ substr($hoteldate->descr, 0,3) }}</span>
@@ -17,3 +21,4 @@
 
   {{ Form::button('Update >',['class'=>'updatetoday btn btn-primary btn-lg','title2'=>\Carbon\Carbon::parse($date2)->format('Y-m-d'),'title3'=>((isset($order)?$order:'TW'))]) }}
 </fieldset>
+
