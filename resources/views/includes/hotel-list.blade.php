ac5be9df29c9cd7e52b5b300caf10db2c61b7fe1
bas2 <me@bashir.biz>
2017-04-14 17:02:36 +0100

    Moved code from Welcome blade.

--- resources/views/includes/hotel-list.blade.php
+++ resources/views/includes/hotel-list.blade.php
@@ -1,5 +1,9 @@
+<h5 class="text-center">{{ \Carbon\Carbon::parse($day)->format('l, j F') }} check-in</h5>
+
+<p class="text-center">Online booking available until <strong>midnight on {{ \Carbon\Carbon::parse($day)->addDay()->format('l') }}</strong>, <strong>check-in is after 3pm</strong> and <strong>check-out is at midday on {{ \Carbon\Carbon::parse($day)->addDay()->format('l') }}</strong>.</p>
+
 @if(!count($hotels))
-<p class="alert alert-info text-center">No cheap hotels on {{ $day }}</p>
+<p class="alert alert-info text-center">No cheap hotels on {{ \Carbon\Carbon::parse($day)->format('l') }}</p>
 @else
 <ul class="list-group">
   @foreach($hotels as $hoteldate)
