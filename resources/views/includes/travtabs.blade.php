bc2796f2abbf822041920767e9081e5db34f9887
117229 <u88610319@cbi-bs1.server.lan>
2017-04-21 20:48:10 +0200

    Commit.

--- resources/views/includes/travtabs.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- Nav tabs -->
-<ul class="nav nav-tabs" role="tablist" title2="{{ \Carbon\Carbon::parse($date2)->format('Y-m-d') }}" title3="{{ $text }}">
-  <li role="presentation"><a href="#wal" aria-controls="wal" role="tab" data-toggle="tab">WAL</a></li>
-  <li role="presentation"><a href="#tol" aria-controls="tol" role="tab" data-toggle="tab">TOL</a></li>
-  <li role="presentation" class="active"><a href="#waltol" aria-controls="waltol" role="tab" data-toggle="tab">WALTOL</a></li>
-</ul>
