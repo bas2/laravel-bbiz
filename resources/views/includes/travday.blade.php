03860c2ad8d4b4964e549cefc21e273adb8c82d7
bas2 <me@bashir.biz>
2017-04-21 19:33:12 +0100

    Added tabs ul. This will only be shown on initial page load and not when reloading via ajax. Added the div around fieldset that used to be in update blade. Changed price range to 50 from 70. Added .new-price class to div.

--- resources/views/includes/travday.blade.php
+++ resources/views/includes/travday.blade.php
@@ -1,23 +1,34 @@
-<fieldset>
-  <legend>{{ $text }} {{ \Carbon\Carbon::parse($date2)->format('l, j F') }}</legend>
-  
-  @foreach($travelodge[strtolower(str_replace(' ','',$text))] as $hoteldate)
-  @if( !isset($order) # Initial.
-  || ( $order == 'WALTOL' ) # All.
-  || ( in_array($order, [strtoupper(substr($hoteldate->descr, 0,3))]) ) # 
-  || ( 'WALTOL' == strtoupper($hoteldate->descr) ) # 
-  )
-  <div class="form-group">
-  {!! Form::select("price_{$hoteldate->date_id}",range(0,70),$hoteldate->price) !!}
-  {{ Form::label($hoteldate->hotels[0]->name) }} <span class="zero">x</span> <span class="hoteldescr">{{ substr($hoteldate->descr, 0,3) }}</span>
-  </div>
-  @endif
-  @endforeach
+@if(!isset($dontshowtabs))
+<!-- Nav tabs -->
+<ul class="nav nav-tabs" role="tablist" title2="{{ \Carbon\Carbon::parse($date2)->format('Y-m-d') }}" title3="{{ $text }}">
+  <li role="presentation"><a href="#wal" aria-controls="wal" role="tab" data-toggle="tab">WAL</a></li>
+  <li role="presentation"><a href="#tol" aria-controls="tol" role="tab" data-toggle="tab">TOL</a></li>
+  <li role="presentation" class="active"><a href="#waltol" aria-controls="waltol" role="tab" data-toggle="tab">WALTOL</a></li>
+</ul>
+@endif
 
-  <div class="form-group">
-  {!! Form::select("price_",range(0,70),(isset($price)?$price:39)) !!}
-  {!! Form::select("hotel",$travelodge['hotels'],'',['class'=>'addhotel']) !!}
-  </div>
+<div>
+  <fieldset>
+    <legend>{{ $text }} {{ \Carbon\Carbon::parse($date2)->format('l, j F') }}</legend>
 
-  {{ Form::button('Update >',['class'=>'updatetoday btn btn-primary','title2'=>\Carbon\Carbon::parse($date2)->format('Y-m-d'),'title3'=>((isset($order)?$order:'TW'))]) }}
-</fieldset>
+    @foreach($travelodge[strtolower(str_replace(' ','',$text))] as $hoteldate)
+      @if( !isset($order) # Initial.
+      || ( $order == 'WALTOL' ) # All.
+      || ( in_array($order, [strtoupper(substr($hoteldate->descr, 0,3))]) ) # 
+      || ( 'WALTOL' == strtoupper($hoteldate->descr) ) # 
+      )
+      <div class="form-group">
+      {!! Form::select("price_{$hoteldate->date_id}",range(0,50),$hoteldate->price) !!}
+      {{ Form::label($hoteldate->hotels[0]->name) }} <span class="zero">x</span> <span class="hoteldescr">{{ substr($hoteldate->descr, 0,3) }}</span>
+      </div>
+      @endif
+    @endforeach
+
+    <div class="form-group new-price">
+    {!! Form::select("price_",range(0,50),(isset($price)?$price:39)) !!}
+    {!! Form::select("hotel",$travelodge['hotels'],'',['class'=>'addhotel']) !!}
+    </div>
+
+    {{ Form::button('Update >',['class'=>'updatetoday btn btn-primary','title2'=>\Carbon\Carbon::parse($date2)->format('Y-m-d'),'title3'=>((isset($order)?$order:'TW'))]) }}
+  </fieldset>
+</div>
