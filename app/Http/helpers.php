f6748dda8146d914cd5a39bae0e22a1f3495cb85
bas2 <me@bashir.biz>
2017-04-24 20:26:05 +0100

    week.

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -2,8 +2,9 @@
 
 function FixData($data){
   $str=str_replace(' before', '', $data);
-  $str=str_replace(' hours', 'h', $str);
-  $str=str_replace('week', 'w', $str);
+  $str=str_replace(' hour', 'h', $str);
+  $str=str_replace('hs', 'h', $str);
+  $str=str_replace(' week', 'wk', $str);
   $str=str_replace(' day', 'd', $str);
   $str=str_replace('ds', 'd', $str);
   return $str;
