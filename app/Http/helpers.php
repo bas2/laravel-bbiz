88f36d54ace0966637214002137bec344c763ba8
bas2 <me@bashir.biz>
2017-04-25 19:02:51 +0100

    Added minutes.

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -4,6 +4,7 @@ function FixData($data){
   $str=str_replace(' before', '', $data);
   $str=str_replace(' hour', 'h', $str);
   $str=str_replace('hs', 'h', $str);
+  $str=str_replace(' minutes', 'm', $str);
   $str=str_replace(' week', 'wk', $str);
   $str=str_replace(' day', 'd', $str);
   $str=str_replace('ds', 'd', $str);
