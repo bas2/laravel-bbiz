4efe18e476c84a8b4a232ded48999f1dd471c421
bas2 <me@bashir.biz>
2017-05-22 01:12:05 +0100

    New commit.

---
+++ resources/views/pages/viewmessages.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+@section('content')
+
+<div class="container-fluid">
+<div class="jumbotron">
+
+<ul>
+@foreach($messages as $message)
+@if($loop->first)
+<li>{{ $message->message }} posted by <strong>Bashir</strong> on <strong>{{ $message->created_at }}</strong></li>
+@else
+<li>{{ $message->message }} posted by <strong>{{ $message->name }}</strong> on <strong>{{ $message->created_at }}</strong></li>
+@endif
+@endforeach
+</ul>
+
+{{ Form::open() }}
+
+<div class="form-group">
+{{ Form::textarea('message',null,['class'=>'form-control','placeholder'=>'Your message...']) }}
+</div>
+@foreach($errors->all() as $error)
+<p class="alert alert-warning">{{ $error }}</p>
+@endforeach
+@if($flash=session('failuremessage'))
+<p class="alert alert-danger">{{ $flash }}</p>
+@endif
+<div class="form-group">
+{!! Form::submit('Reply &rarr;',['class'=>'btn btn-primary']) !!}
+</div>
+
+{{ Form::close() }}
+
+</div>
+</div>
+@endsection
