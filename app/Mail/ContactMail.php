ef508ced2a9a3eb32987681cea92d645124d7d70
bas2 <me@bashir.biz>
2017-05-22 21:33:57 +0100

    2 space indentation.

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -9,29 +9,29 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 
 class ContactMail extends Mailable
 {
-    private $msg=[];
-    use Queueable, SerializesModels;
+  private $msg=[];
+  use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct($name,$email,$msg)
-    {
-      $this->msg=[$name,$email,$msg];
-    }
+  /**
+    * Create a new message instance.
+    *
+    * @return void
+    */
+  public function __construct($name,$email,$msg)
+  {
+    $this->msg=[$name,$email,$msg];
+  }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-      // Name, email, message.
-      return $this->view('emails.contact')
-      ->with('cont',[$this->msg[0],$this->msg[1],$this->msg[2]]);
-    }
+  /**
+    * Build the message.
+    *
+    * @return $this
+    */
+  public function build()
+  {
+    // Name, email, message.
+    return $this->view('emails.contact')
+    ->with('cont',[$this->msg[0],$this->msg[1],$this->msg[2]]);
+  }
 }
 
