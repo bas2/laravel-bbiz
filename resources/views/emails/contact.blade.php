722d1f4bb0d5c3ead74c26596979078bd956735e
bas2 <me@bashir.biz>
2017-05-22 01:03:52 +0100

    Added condition to not show empty message text in email.

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,8 +1,14 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <body>
-<h1>New message from {{ $cont[0] }}</h1>
-<p>{{ $cont[1] }}</p>
-<p>{{ $cont[2] }}</p>
+
+  <h1>New message from {{ $cont[0] }}</h1>
+
+  <p>{{ $cont[1] }}</p>
+
+  @if(!empty($cont[2]))
+  <p>{{ $cont[2] }}</p>
+  @endif
+
 </body>
 </html>
